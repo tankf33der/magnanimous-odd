8cacffe98a42dbed672561b44b9bc3826d612695
Mike <tankf33der@disroot.org>
2021-02-25 18:39:15 +0200

    .

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
 	dmd -release -O -boundscheck=off odds.d
+c:
+	gcc -O3 -lm -lgmp odds.c
 clean:
 	rm -rf odds *.o	
--- odds.c
+++ odds.c
@@ -2,25 +2,86 @@
 #include <math.h>
 #include "gmp.h"
 
-unsigned char a$;
+int a$;
+
+void push(char* s, const char* a) {
+	for(int i = 0; a$ > i; i++) {
+		s[i] = a[i] + 48;
+	}
+} 
+
+int ismagna(const char* s) {
+	mpz_t N, D, N10, Q, R;
+
+	mpz_init(N);
+	mpz_init_set_ui(D, 10);
+	mpz_init_set_ui(N10, 10);
+	mpz_init(Q);
+	mpz_init(R);
+
+	mpz_set_str(N, s, 10);
+	for(int i = 0; a$-1 > i; i++) {
+		mpz_tdiv_qr (Q, R, N, D);
+		mpz_add(Q, Q, R);
+		if (!mpz_probab_prime_p(Q, 16)) {
+			mpz_clear(N);
+			mpz_clear(D);
+			mpz_clear(N10);
+			mpz_clear(Q);
+			mpz_clear(R);
+			return 0;
+		}
+		mpz_mul(D, D, N10);
+	}
+
+	mpz_clear(N);
+	mpz_clear(D);
+	mpz_clear(N10);
+	mpz_clear(Q);
+	mpz_clear(R);
+	return 1;
+}
 
 int main(void) {
 	// XXX fun starts here
-	unsigned char a[4] = {1};
-	unsigned char s[4];
+	char a[12];
+	char s[13];	// a+1
 	// ===
 	
 	a$ = sizeof(a);
+	for(int i = 0; a$ > i; i++) {
+		a[i] = 1;
+	}
 	a[a$-1] = 0;
-	
+	s[a$] = 0;
 
 	// XXX (** 5 a$), max 27
-	for(unsigned long i = 0; 625 > i; i++) {
-
+	for(unsigned long i = 0; 244140625 > i; i++) {
+		push(s, a);
+		// XXX, s is ready
+		// printf("%s, %d\n", s, ismagna(s));
+		if (ismagna(s)) {
+			printf("%s\n", s);
+		}
+		// ===
+		
+		// next
+		a[a$-1] += 2;
+		if (a[a$-1] <= 8) {
+			continue;
+		} else {
+			a[a$-1] = 0;
+		}
+		for(int j = a$-2; j >= 0; j--) {
+			a[j] += 2;
+			if (a[j] <= 9) {
+				break;
+			}
+			else {
+				a[j] = 1;
+			}
+		}
 	}
-	s[0] = a[0] + 48;
-	printf("'%c'\n", s[0]);
-	
-	
+
 	return 0;
 }
