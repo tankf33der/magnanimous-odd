49391695254241db4bf565aa6c1f58a8419f03d8
Mike <tankf33der@disroot.org>
2021-02-25 17:28:33 +0200

    .

---
+++ odds.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include "gmp.h"
+
+unsigned char a$;
+
+int main(void) {
+	// XXX fun starts here
+	unsigned char a[4] = {1};
+	unsigned char s[4];
+	// ===
+	
+	a$ = sizeof(a);
+	a[a$-1] = 0;
+	
+
+	// XXX (** 5 a$), max 27
+	for(unsigned long i = 0; 625 > i; i++) {
+
+	}
+	s[0] = a[0] + 48;
+	printf("'%c'\n", s[0]);
+	
+	
+	return 0;
+}
