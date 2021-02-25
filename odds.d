b8d2a9648cd0135bcde642503b97f2ca0101ed89
Mike <tankf33der@disroot.org>
2021-02-25 10:06:32 +0200

    .

--- odds.d
+++ odds.d
@@ -1,10 +1,76 @@
 import std.stdio;
 import std.bigint;
 import std.math;
+import std.format;
+import std.random;
+
+// XXX
+ubyte NDigits = 0;
+
+// Input: n is always > 2 and odd
+// easy probably prime based on Miller-Rabin
+// works on n > 1024
+bool isPrime(in BigInt n)
+{
+    // if (n > 2 && !(n & 1))
+    // {
+        // return false;
+    // }
+// 
+    // if (n < 10000){
+        // return primes.canFind(n);
+    // }
+
+	BigInt d = n - 1;
+	ulong s = 0;
+	while(!(d & 1))
+	{
+		d /= 2;
+		s++;
+	}
+
+	outer:
+	foreach (immutable _; 0..8)
+	{
+		ulong a = uniform(2, 1024);
+		BigInt b = n / a;
+		BigInt x = powmod(b, d, n);
+		if (x == 1 || x == n - 1)
+			continue;
+		foreach (immutable __; 1 .. s)
+		{
+			x = powmod(x, BigInt(2), n);
+			if (x == 1)
+				return false;
+			if (x == n - 1)
+				continue outer;
+		}
+		return false;
+	}
+	return true;
+}
+    bool isMagna(in BigInt n)
+    {
+        BigInt p = 10;
+        while (n >= p)
+        {
+            auto q = n / p;
+            auto r = n % p;
+            if(!isPrime(q + r))
+            {
+                return false;
+            }
+            p *= 10;
+        }
+        return true;
+    }
+
+
 
 void main() {
-	ubyte[15] a;
-	byte NDigits = a.length;
+	// XXX, size
+	ubyte[10] a;
+	NDigits = a.length;
 
 	for(int i = 0; a.length-2 > i; i++) {
 		a[i] = 1;
@@ -13,7 +79,9 @@ void main() {
 	auto E = pow(5, NDigits);
 	for(int i = 0; E > i; i++) {
 		// number is here
-		// writeln(a);
+		if (isMagna(BigInt(format!"%(%s%)"(a)))) {
+			writeln(format!"%(%s%)"(a));
+		}
 		// ====
 		a[$-1] += 2;
 		if (a[$-1] <= 8) {
@@ -32,7 +100,6 @@ void main() {
 		}		
 		
 	}
-	// auto s = a.map!(a => a+1));
-	writeln("mike");
+	writeln("fin.");
 }
 
