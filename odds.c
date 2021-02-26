dce45511695236740aa6420f825853147ab8ae26
Mike <tankf33der@disroot.org>
2021-02-26 06:56:38 +0200

    .

--- odds.c
+++ odds.c
@@ -44,8 +44,8 @@ int ismagna(const char* s) {
 
 int main(void) {
 	// XXX fun starts here
-	char a[15];
-	char s[16];	// a+1
+	char a[16];
+	char s[17];	// a+1
 	// ===
 	
 	a$ = sizeof(a);
@@ -56,7 +56,7 @@ int main(void) {
 	s[a$] = 0;
 
 	// XXX (** 5 a$), max 27
-	for(unsigned long i = 0; 30517578125 > i; i++) {
+	for(unsigned long i = 0; 152587890625 > i; i++) {
 		push(s, a);
 		// XXX, s is ready
 		// printf("%s, %d\n", s, ismagna(s));
